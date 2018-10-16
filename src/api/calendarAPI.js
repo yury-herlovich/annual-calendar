b77d4466ba2ce91d005693eeefe539cee542dd66
Yury Herlovich <yuryherlovich@gmail.com>
2018-10-16 11:22:20 -0700

    Save google token in the local storage

--- src/api/calendarAPI.js
+++ src/api/calendarAPI.js
@@ -2,6 +2,7 @@ const CLIENT_ID = process.env.REACT_APP_CALENDAR_CLIENT_ID;
 const API_KEY = process.env.REACT_APP_CALENDAR_API_KEY;
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 //const CALENDAR_ID = process.env.REACT_APP_CALENDAR_ID;
+const STORAGE_TOKEN_DATA = 'googleAPIToken';
 
 // Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
 const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
@@ -20,6 +21,13 @@ export function initCalendarAPI() {
 function initClient() {
   var gapi = window['gapi'];
 
+  let token = checkTokenInStorage();
+
+  if (token) {
+    gapi.client.setToken(token);
+    return;
+  }
+
   gapi.client.init({
     apiKey: API_KEY,
     clientId: CLIENT_ID,
@@ -28,8 +36,33 @@ function initClient() {
   }).then(function () {
     return gapi.auth2.getAuthInstance().signIn();
   }).then(user => {
-    console.log(user);
+    saveTokenToStorage(user.Zi);
   }).catch((err) => {
     console.log(err);
   });
+}
+
+
+function saveTokenToStorage(tokenData) {
+  localStorage.setItem(STORAGE_TOKEN_DATA, JSON.stringify(tokenData));
+}
+
+
+function checkTokenInStorage() {
+  let tokenString = localStorage.getItem(STORAGE_TOKEN_DATA);
+
+  if (tokenString === null) return null;
+
+  try {
+    let token = JSON.parse(tokenString);
+
+    if (token.expires_at <= Date.now()) {
+      throw new Error('Expired token');
+    }
+
+    return token;
+  } catch (err) {
+    localStorage.removeItem(STORAGE_TOKEN_DATA);
+    return null;
+  }
 }
