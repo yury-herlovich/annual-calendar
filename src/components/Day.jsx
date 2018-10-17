9763404b16303ca62dc052ebcb37af126424da13
Yury Herlovich <yuryherlovich@gmail.com>
2018-10-17 12:43:26 -0700

    Select today

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import moment from 'moment';
 
-const Day = ({item}) => (
-  <div className="day">
-    <header className="day-header">{moment(item.date).format('ddd/DD')}</header>
-  </div>
-);
+const Day = ({item}) => {
+  return (
+    <div className={'day' + (moment().startOf('date').diff(item.date, 'hours') === 0 ? ' today' : '')}>
+      <header className="day-header">{moment(item.date).format('ddd/DD')}</header>
+    </div>
+  )
+};
 
 export default Day;
