066e2196192e18907dd97529a857ed73f442a106
Yury Herlovich <yuryherlovich@gmail.com>
2018-10-16 18:17:42 -0700

    Add function for generation of calendar

---
+++ src/utils/utils.js
@@ -0,0 +1,26 @@
+import moment from 'moment';
+
+export function generateCalendar(year) {
+  let calendar = [];
+  let date = moment({ year, month: 0 , day: 1});
+
+  for (let i = 0; i < 12; i++) {
+    let month = {
+      id: `${year}${date.format('MM')}`,
+      days: []
+    };
+
+    // add days
+    while (date.get('month') === i) {
+      month.days.push({
+        id: `${year}${date.format('MM')}${date.format('DD')}`,
+        events: []
+      });
+      date.add(1, 'day');
+    }
+
+    calendar.push(month);
+  }
+
+  return calendar;
+}
