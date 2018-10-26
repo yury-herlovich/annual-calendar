68fcdc3cf396e3cc7d39894f8ac53bbe76d55914
Yury Herlovich <yuryherlovich@gmail.com>
2018-10-26 15:18:27 -0700

    Move add/edit form render to presentational component

--- src/components/AddEditEvent/AddEditEvent.jsx
+++ src/components/AddEditEvent/AddEditEvent.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import moment from 'moment';
 
-import Form from '../Form/Form';
-import Input from '../Form/Input';
-import Checkbox from '../Form/Checkbox';
-import Button from '../Form/Button';
+import AddEditForm from './AddEditForm';
 
 import './AddEditEvent.css';
 import { getEvent } from '../../actions/calendarActions';
@@ -88,60 +84,11 @@ class AddEditEvent extends Component {
 
     return (
       <main>
-        <Form handleSubmit={this.handleSubmit} className="form add-edit-form">
-          <div className='form-group'>
-            <Input
-              type='text'
-              name='title'
-              placeholder='Add title'
-              value={this.state.title}
-              handleChange={this.handleInputChange} />
-          </div>
-
-          <div className='form-group'>
-            <Input
-              type='date'
-              name='startDate'
-              value={this.state.startDate}
-              handleChange={this.handleInputChange} />
-
-            { !this.state.allDay &&
-              <Input
-                type='time'
-                name='startTime'
-                value={this.state.startTime}
-                handleChange={this.handleInputChange} />
-            }
-
-            &nbsp;-&nbsp;
-
-            <Input
-              type='date'
-              name='endDate'
-              value={this.state.endDate}
-              handleChange={this.handleInputChange} />
-
-            { !this.state.allDay &&
-              <Input
-                type='time'
-                name='endTime'
-                value={this.state.endTime}
-                handleChange={this.handleInputChange} />
-            }
-          </div>
-
-          <div className="form-group">
-            <Checkbox name="allDay" value={this.state.allDay} handleChange={this.handleInputChange} />
-            <label htmlFor="allDay">All day</label>
-          </div>
-
-          <div className="form-buttons form-group">
-            <Button name='submit' text='Save' handleClick={this.handleSubmit} />
-            <Link to={'/year/' + this.props.year}>
-              <Button name='cancel' text='Cancel' />
-            </Link>
-          </div>
-        </Form>
+        <AddEditForm
+          eventData={this.state}
+          year={this.props.year}
+          handleSubmit={this.handleSubmit}
+          handleInputChange={this.handleInputChange} />
       </main>
     )
   }
---
+++ src/components/AddEditEvent/AddEditForm.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Form from '../Form/Form';
+import Input from '../Form/Input';
+import Checkbox from '../Form/Checkbox';
+import Button from '../Form/Button';
+
+const AddEditForm = ({eventData, year, handleSubmit, handleInputChange}) => (
+  <Form handleSubmit={handleSubmit} className="form add-edit-form">
+    <div className='form-group'>
+      <Input
+        type='text'
+        name='title'
+        placeholder='Add title'
+        value={eventData.title}
+        handleChange={handleInputChange} />
+    </div>
+
+    <div className='form-group'>
+      <Input
+        type='date'
+        name='startDate'
+        value={eventData.startDate}
+        handleChange={handleInputChange} />
+
+      { !eventData.allDay &&
+        <Input
+          type='time'
+          name='startTime'
+          value={eventData.startTime}
+          handleChange={handleInputChange} />
+      }
+
+      &nbsp;-&nbsp;
+
+      <Input
+        type='date'
+        name='endDate'
+        value={eventData.endDate}
+        handleChange={handleInputChange} />
+
+      { !eventData.allDay &&
+        <Input
+          type='time'
+          name='endTime'
+          value={eventData.endTime}
+          handleChange={handleInputChange} />
+      }
+    </div>
+
+    <div className="form-group">
+      <Checkbox name="allDay" value={eventData.allDay} handleChange={handleInputChange} />
+      <label htmlFor="allDay">All day</label>
+    </div>
+
+    <div className="form-buttons form-group">
+      <Button name='submit' text='Save' handleClick={handleSubmit} />
+      <Link to={'/year/' + year}>
+        <Button name='cancel' text='Cancel' />
+      </Link>
+    </div>
+  </Form>
+);
+
+export default AddEditForm;
