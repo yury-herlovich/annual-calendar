7228c5cf61dabd4736b170df9600fd2a3ad1d8d1
Yury Herlovich <yuryherlovich@gmail.com>
2018-10-26 11:10:03 -0700

    Fix spaces in the for components

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Input = ({type, name, value, handleChange, placeholder}) => (
-    <input
-        type={type}
-        placeholder={placeholder}
-        name={name}
-        id={name}
-        value={value}
-        onChange={handleChange} />
+  <input
+    type={type}
+    placeholder={placeholder}
+    name={name}
+    id={name}
+    value={value}
+    onChange={handleChange} />
 )
 
 export default Input;
