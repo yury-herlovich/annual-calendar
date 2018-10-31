0b7f1e6480e4e91937df7be6851dc33eebbfb8c4
Yury Herlovich <yuryherlovich@gmail.com>
2018-10-31 13:34:27 -0700

    Fix value in the textarea

--- src/components/Form/Textarea.jsx
+++ src/components/Form/Textarea.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Textarea = ({ name, value, handleChange, placeholder, height}) => (
-  <textarea name={name} placeholder={placeholder} onChange={handleChange} style={{height: height}}>
-    {value}
-  </textarea>
+  <textarea
+    name={name}
+    placeholder={placeholder}
+    onChange={handleChange}
+    style={{height: height}}
+    value={value} />
 )
 
 export default Textarea;
